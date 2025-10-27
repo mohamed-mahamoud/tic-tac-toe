2edc272c8986294b0f59073e53c744cd09714582
Mohamed <mohamed.mahamoud0112@gmail.com>
2025-10-27 16:15:38 +0100

    deux joueur fait manque encore quelque modif

--- morpion.py
+++ morpion.py
@@ -16,6 +16,7 @@ def gagne_condition(tableau,tour):
     for i in range(3):
         if tableau[i][0]==tableau[i][1]==tableau[i][2]==symbole or tableau[0][i]==tableau[1][i]==tableau[2][i]==symbole :
             w=True
+            return w
             break
     if tableau[0][0]==tableau[1][1]==tableau[2][2]==symbole or tableau[0][2]==tableau[1][1]==tableau[2][0]==symbole:
         w=True           
@@ -27,15 +28,27 @@ def placement(table,tour):
         while table[x][y]!=" ":
             x,y=map(int,input("erreur cett case est déja jouer choisisez une autre ").split())
         tableau[x][y]="X"
-        return 2
     elif tour==2 :
         x,y=map(int,input(" placer les cordonné de la case que vous vouler cocher ").split())
         while table[x][y]!=" ":
             x,y=map(int,input("erreur cett case est déja jouer choisisez une autre ").split())
-        table[x][y]="O"
-        return 1          
-tour=2
-affichage(tableau)
-tour=placement(tableau,tour)
-affichage(tableau)
-print(tour)
+        table[x][y]="O"        
+joueur=1
+win=False
+tour=0
+while not win and tour<9:
+    placement(tableau,joueur)
+    affichage(tableau)
+    win=gagne_condition(tableau,joueur)
+    if win:
+        print(f"bravo jouer {joueur} à gagner ")
+    else:
+        tour+=1
+    if joueur==1:
+        joueur=2
+    else:
+        joueur=1        
+if not win and tour==9:
+    print("c'est une égalité")            
+    
+#il me reste a poser la securité pour avoir les bonne cordonné et faire le message de début 
