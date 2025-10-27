b7df60ae85fa36f094b5d51f1037e81640e60712
Mohamed <mohamed.mahamoud0112@gmail.com>
2025-10-27 15:35:37 +0100

    la fonction pour que un joueur puisse jouer

--- morpion.py
+++ morpion.py
@@ -1,13 +1,17 @@
-class tableau:
-    def __init__(self):
-        self.dessin=[[" "," "," "],[" "," "," "],[" "," "," "]]
-    def affichage(self):
-        for i in self.dessin:
-            print(f"+{"-"*3}"*3+"+")
-            print (f"|{" "}{i[0]}{" "}|{" "}{i[1]}{" "}|{" "}{i[2]}{" "}|")
-        print(f"+{"-"*3}"*3+"+")
+tableau=[[" "," "," "],[" "," "," "],[" "," "," "]]
 
-def gagne_condition(tableau,symbole):
+def affichage(tableau):
+    for i in tableau:
+        print(f"+{"-"*3}"*3+"+")
+        print (f"|{" "}{i[0]}{" "}|{" "}{i[1]}{" "}|{" "}{i[2]}{" "}|")
+    print(f"+{"-"*3}"*3+"+")
+
+def gagne_condition(tableau,tour):
+    if tour==1:
+        symbole="X"
+    else:
+        symbole="O"    
+    
     w=False
     for i in range(3):
         if tableau[i][0]==tableau[i][1]==tableau[i][2]==symbole or tableau[0][i]==tableau[1][i]==tableau[2][i]==symbole :
@@ -16,12 +20,22 @@ def gagne_condition(tableau,symbole):
     if tableau[0][0]==tableau[1][1]==tableau[2][2]==symbole or tableau[0][2]==tableau[1][1]==tableau[2][0]==symbole:
         w=True           
     return w 
-def placement(tableau,tour):
+def placement(table,tour):
     print(f"c'est le tour du joueur {tour} de joué")
     if tour==1:
-        cord=[int(nb) for nb in  input(" placer les cordonné de la case que vous vouler cocher").split(" ")]
-        while tableau[cord[0]][cord[1]]!=" ":
-            cord=[int(nb) for nb in  input("erreur cett case est déja jouer choisisez une autre ").split(" ")]
-board=tableau()
-board.affichage()
-board.dessin[0][0]
+        x,y=map(int,input(" placer les cordonné de la case que vous vouler cocher ").split())
+        while table[x][y]!=" ":
+            x,y=map(int,input("erreur cett case est déja jouer choisisez une autre ").split())
+        tableau[x][y]="X"
+        return 2
+    elif tour==2 :
+        x,y=map(int,input(" placer les cordonné de la case que vous vouler cocher ").split())
+        while table[x][y]!=" ":
+            x,y=map(int,input("erreur cett case est déja jouer choisisez une autre ").split())
+        table[x][y]="O"
+        return 1          
+tour=2
+affichage(tableau)
+tour=placement(tableau,tour)
+affichage(tableau)
+print(tour)
